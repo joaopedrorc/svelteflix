ad1c18b7a648b5b651f3aab8de4221171e952da7
João Pedro Cordeiro <jpc5626@gmail.com>
2024-02-20 22:34:30 -0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Hero from './Hero.svelte';
+	import Carousel from '$lib/components/Carousel.svelte';
+	export let data;
+
+	console.log(data);
+</script>
+
+<h1 class="column">Top trending movies</h1>
+
+<Hero movie={data.featured} />
+
+<Carousel movies={data.trending.results} />
